b77495f5e22acf201fddb483077b4f84bab5f061
itwasjoke <andreyvasiliev1204@gmail.com>
2024-03-01 20:25:55 +0300

    members try

--- src/views/members.vue
+++ src/views/members.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
     <Header title-name="Участники" title-description="изменение прав" />
+    <MemberListBlock header="Администраторы" type="admins"/>
+    <MemberListBlock header="Администраторы" type="admins"/>
+    <MemberListBlock header="Администраторы" type="admins"/>
   </div>
 </template>
-
-
-
-
 <script>
+
+
+// TODO 1 Сделать компонент с блоком участников MemberListBlock
+
+// props: заголовок, тип
+// Весь компонент оборачиваем в div
+// Заголовок добавляем h3
+// Добавляем список из компонента 2
+
+
+
+// TODO 2 Создаем компонент для одного участника MemberListElement
+
+// props: имя и тип (который был до этого)
+// оборачиваем в div
+// делаем текст для имени <p>
+// делаем div для трех точек
+
 import Header from "@/components/header.vue";
 export default {
   name: 'Members',
