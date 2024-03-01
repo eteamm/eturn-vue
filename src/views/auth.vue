02e0e0ff1713d706609eaa770e9ba3bf5babfb16
itwasjoke <andreyvasiliev1204@gmail.com>
2024-03-01 23:15:59 +0300

    auth animation

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -3,10 +3,11 @@
     <Header title-name="Вход" title-description="Введите данные для авторизации" />
     <input v-model="login" name="login" placeholder="Введите логин" class="NameBox">
     <input v-model="password" name="password" placeholder="Введите пароль" class="NameBox" type="password">
-    <button class="classic_button" v-on:click="authUser">войти</button>
+    <button class="classic_button" v-bind:class="{animation_auth: getLoading('auth_loading')}" v-on:click="authUser">войти</button>
     <p v-if="getCurrentError('auth_error')" class="errorInfoAuth">Логин или пароль введены неверно.</p>
   </div>
 </template>
+
 
 <script>
 import Header from "@/components/header.vue";
@@ -18,7 +19,7 @@ export default {
     Header
   },
   computed:{
-    ...mapGetters(['getterToken', 'getCurrentError'])
+    ...mapGetters(['getterToken', 'getCurrentError', 'getLoading'])
   },
   mounted() {
     let token = this.$store.getters.getterToken
