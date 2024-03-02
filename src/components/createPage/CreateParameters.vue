1c479deeb4d1703d5868f8259031668c6d8384a5
ZenBook <nikitashepel5@gmail.com>
2024-03-02 17:04:18 +0300

    creategroup

--- src/components/createPage/CreateParameters.vue
+++ src/components/createPage/CreateParameters.vue
@@ -4,11 +4,11 @@
 
     <select v-model="choice" class = "ParameterBox">
       <option class = "ParameterText" disabled value="">Выберите параметр: </option>
-      <option>Параметр A</option>
-      <option>Параметр B</option>
-      <option>Параметр C</option>
-      <option>Параметр D</option>
-      <option>Параметр E</option>
+      <option>    Параметр A</option>
+      <option>    Параметр B</option>
+      <option>    Параметр C</option>
+      <option>    Параметр D</option>
+      <option>    Параметр E</option>
     </select>
   </div>
 </template>
