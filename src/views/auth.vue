8c2968c45e88ad09e46a31e3cc81f8706c3b6f12
itwasjoke <andreyvasiliev1204@gmail.com>
2024-03-03 15:20:13 +0300

    paddings

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <Header title-name="Вход" title-description="Введите данные для авторизации" />
-    <input v-model="login" name="login" placeholder="Введите логин" class="NameBox authInput">
-    <input v-model="password" name="password" placeholder="Введите пароль" class="NameBox" type="password">
-    <button class="classic_button" v-bind:disabled="isClicked" v-bind:class="{animation_auth: getLoading('auth_loading'), classic_button_add: isClicked}" v-on:click="authUser">войти</button>
-    <p v-if="getCurrentError('auth_error')" class="errorInfoAuth">Логин или пароль введены неверно.</p>
-  </div>
+<!--  <div class="container">-->
+<!--    <div class="row justify-content-center">-->
+<!--      <div class="col-md-12">-->
+        <div>
+          <Header title-name="Вход" title-description="Введите данные для авторизации" />
+          <input v-model="login" name="login" placeholder="Введите логин" class="NameBox authInput">
+          <input v-model="password" name="password" placeholder="Введите пароль" class="NameBox" type="password">
+          <button class="classic_button" v-bind:disabled="isClicked" v-bind:class="{animation_auth: getLoading('auth_loading'), classic_button_add: isClicked}" v-on:click="authUser">войти</button>
+          <p v-if="getCurrentError('auth_error')" class="errorInfoAuth">Логин или пароль введены неверно.</p>
+        </div>
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script>
